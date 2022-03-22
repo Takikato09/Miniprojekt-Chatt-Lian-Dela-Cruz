b1128082fcd5c8f6f6d3ef39f8f0148b7bb30a48
Takikato09 <liandelacruz09@hotmail.com>
2022-03-22 13:37:27 +0100

    installed firebase / codes
    added some codes from the database to my databaseApp.ts

---
+++ src/modules/databaseApp.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAfSm8OxOIIpg2IrQJFQ-ryGbYmRenPsZo",
+  authDomain: "chatt-box-mini-project.firebaseapp.com",
+  projectId: "chatt-box-mini-project",
+  storageBucket: "chatt-box-mini-project.appspot.com",
+  messagingSenderId: "573299552249",
+  appId: "1:573299552249:web:f3a4b73f0423ea44341728"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
